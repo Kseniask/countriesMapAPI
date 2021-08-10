17938bbad1c7aed3f060f341c92ef8b2fac91e7b
kseniask <kseniask30@icloud.com>
2021-08-09 19:44:21 -0700

    add CORS header

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express')
 
 const app = express()
 app.use(express.json())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  next()
+})
 
 const countriesRouter = require('./routes/countries')
 
